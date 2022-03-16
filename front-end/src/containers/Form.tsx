68db41b285dbdb837f34e5e03661d50589bbf081
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-16 12:22:36 +0100

    adding form for new module

--- front-end/src/containers/Form.tsx
+++ front-end/src/containers/Form.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import NewModule from '../components/NewModule'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="min-h-full">
+        <NavBar />
+        
+        <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl font-bold text-gray-900">
+                    Adding new module
+                </h1>
+            </div>
+        </header>
+
+        <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div className="px-4 py-6 sm:px-0">
+                    <NewModule />
+                </div>
+            </div>
+        </main>
+    </div>
   )
 }
 
