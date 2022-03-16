e1f8b0a3b0f67be8c7ed8a73df17c383d52d47cc
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-16 11:20:21 +0100

    adding table, component, container

---
+++ front-end/src/components/TableBody.tsx
@@ -0,0 +1,28 @@
+const TableBody = () => {
+  return (
+    <tbody>
+        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Module bidule
+            </td>
+            <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                500
+            </td>
+            <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                1 days
+            </td>
+            <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                1111
+            </td>
+            <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                OK
+            </td>
+            <td className="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                Graph
+            </td>
+        </tr>
+    </tbody>
+  )
+}
+
+export default TableBody
---
+++ front-end/src/containers/Dashboard.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+import Table from '../components/Table'
+import NavBar from '../components/NavBar'
+import Form from "./Form";
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-full">
+        <NavBar />
+        <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl font-bold text-gray-900">
+                    Dashboard
+                </h1>
+            </div>
+        </header>
+
+        <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    {/* <Link to="/form">
+                        Add new
+                    </Link> */}
+                </button>
+                
+                <div className="px-4 py-6 sm:px-0">
+                    <Table />
+                </div>
+
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Dashboard
