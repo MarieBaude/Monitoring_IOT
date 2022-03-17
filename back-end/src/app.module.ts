87a6b3ba7baf05d658c2f5b3b194fc6dc4de07af
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-17 13:41:42 +0100

    adding detail module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModuleModule } from './module/module.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DetailModule } from './detail/detail.module';
 
 @Module({
-  imports: [ModuleModule, PrismaModule],
+  imports: [ModuleModule, PrismaModule, DetailModule],
   controllers: [AppController],
   providers: [AppService],
 })
