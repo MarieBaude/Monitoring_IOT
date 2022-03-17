3248423c92407fb68c855786d51e3d10ec7c58df
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-17 13:42:29 +0100

    adding log module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ModuleModule } from './module/module.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DetailModule } from './detail/detail.module';
+import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [ModuleModule, PrismaModule, DetailModule],
+  imports: [ModuleModule, PrismaModule, DetailModule, LogModule],
   controllers: [AppController],
   providers: [AppService],
 })
